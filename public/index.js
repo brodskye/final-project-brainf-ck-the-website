dfaa5f6e9cfea8b90dfd907c54f64276d0ecc413
brodskye <brodskye@oregonstate.edu>
2017-06-04 14:09:01 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,70 +1,90 @@
+
 var outputBox = document.getElementById('outputBox');
-
-//initializing the array that hessro will manipulate as a global variable
 var dataArr = [];
-for(var ii = 0; ii < 16; ii++)
-{
-    dataArr.push(32);
-}
 
-//initializing the iterator as a global variable
+var output = "";
 var iter = 0;
 
-function rob()
-{
-    iter = (iter+1)%16;
+//create array
+for (var ii = 0; ii < 16; ii++) {
+    dataArr.push(65);
 }
 
-function hess()
-{
-    iter = (iter-1)%16;
+//Hessro functions
+function rob() {
+    iter = (iter + 1);
 }
 
-function ROB()
-{
+function hess() {
+    iter = (iter - 1); 
+}
+
+function ROB() {
     dataArr[iter]++;
 }
 
-function HESS()
-{
+function HESS() {
     dataArr[iter]--;
 }
 
-var output = "";
-function robhess()
-{
-    output = output + ( String.fromCharCode(dataArr[iter]) );
+function robhess() {
+    output = output + (String.fromCharCode(dataArr[iter]));
 }
 
+function ROBHESSplus() {
+    if (dataArr[iter] != null && dataArr[iter - 1] != null && dataArr[iter - 2] != null) {
+        dataArr[iter] = dataArr[iter - 1] + dataArr[iter - 2];
+    }
+}
 
-ROB();
-rob();
-rob();
-ROB();
-rob();
-rob();
-ROB();
-rob();
-rob();
-ROB();
-hess();
-hess();
-hess();
-hess();
-hess();
-hess();
-robhess();
-rob();
-robhess();
-rob();
-robhess();
-rob();
-robhess();
-rob();
-robhess();
-rob();
-robhess();
+function ROBHESSminus() {
+    if (dataArr[iter] != null && dataArr[iter - 1] != null && dataArr[iter - 2] != null) {
+        dataArr[iter] = dataArr[iter - 1] - dataArr[iter - 2];
+    }
+}
 
-//send array to output
+//interpret the input in inputBox
+function interpret(input) {
+    output = "";
+    dataArr = [];
+    for (var ii = 0; ii < 16; ii++) {
+        dataArr.push(65);
+    }
+    var i = 0;
+    while (i < input.length) {
+        if (input.substring(i, i + 3) == "rob" || input.substring(i, i + 3) == 13) {
+            rob();
+            i += 3;
+        } else if (input.substring(i, i + 3) == "ROB") {
+            ROB();
+            i += 3;
+        } else if (input.substring(i, i + 4) == "hess") {
+            hess();
+            i += 4;
+        } else if (input.substring(i, i + 4) == "HESS") {
+            HESS();
+            i += 4;
+        } else if (input.substring(i, i + 8) == "!robhess") {
+            robhess();
+            i += 8;
+        } else if (input.substring(i, i + 8) == "ROB+HESS") {
+            ROBHESSplus();
+            i += 8;
+        } else if (input.substring(i, i + 8) == "ROB-HESS") {
+            ROBHESSminus();
+            i += 8;
+        } else {
+            i++;
+        }
+    }
+}
 
-outputBox.textContent = output;
+//listen for a click on the compileButton
+document.getElementById('compileButton').addEventListener('click', function () { 
+    interpret(document.getElementById('inputBox').value);
+    outputBox.setAttribute('placeholder', '');
+    outputBox.textContent = output;
+    iter = 0;
+
+});
+
